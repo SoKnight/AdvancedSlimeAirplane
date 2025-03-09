1898a35388dcc94e4a9b04ad67e6cca052acd6bc
SoKnight <mcliyanage2018@gmail.com>
2025-03-10 00:52:10 +0500

    Add paper-mojangapi dependency

--- subprojects/server.gradle.kts
+++ subprojects/server.gradle.kts
@@ -5,4 +5,8 @@ repositories {
 dependencies {
     implementation("com.infernalsuite.aswm:api")
     implementation("com.infernalsuite.aswm:core")
+
+    implementation("com.destroystokyo.paper:paper-mojangapi:1.16.5-R0.1-SNAPSHOT") {
+        exclude("com.destroystokyo.paper", "paper-api")
+    }
 }
