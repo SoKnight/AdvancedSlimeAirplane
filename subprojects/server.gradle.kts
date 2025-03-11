81a03766d64d78030bf4f7964791febfc61fe14a
SoKnight <mcliyanage2018@gmail.com>
2025-03-12 01:28:05 +0500

    ASWM implementation prototype

--- subprojects/server.gradle.kts
+++ subprojects/server.gradle.kts
@@ -1,7 +1,3 @@
-repositories {
-
-}
-
 dependencies {
     implementation("com.infernalsuite.aswm:api")
     implementation("com.infernalsuite.aswm:core")
@@ -9,4 +5,7 @@ dependencies {
     implementation("com.destroystokyo.paper:paper-mojangapi:1.16.5-R0.1-SNAPSHOT") {
         exclude("com.destroystokyo.paper", "paper-api")
     }
+
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
 }
