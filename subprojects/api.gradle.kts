5adc9402840d6d238585a42daed25fc4131bf57d
SoKnight <mcliyanage2018@gmail.com>
2025-03-13 01:32:35 +0500

    Fix core + plugin working together

--- subprojects/api.gradle.kts
+++ subprojects/api.gradle.kts
@@ -2,10 +2,9 @@ java {
     withJavadocJar()
 }
 
-repositories {
-
-}
-
 dependencies {
     api("com.infernalsuite.aswm:api")
+
+    api("net.kyori:adventure-nbt:4.7.0")
+    compileOnlyApi("org.checkerframework:checker-qual:3.49.0")
 }
--- subprojects/server.gradle.kts
+++ subprojects/server.gradle.kts
@@ -1,10 +1,12 @@
 dependencies {
-    implementation("com.infernalsuite.aswm:api")
+    implementation(projects.advancedslimeairplaneApi)
     implementation("com.infernalsuite.aswm:core")
 
     implementation("com.destroystokyo.paper:paper-mojangapi:1.16.5-R0.1-SNAPSHOT") {
         exclude("com.destroystokyo.paper", "paper-api")
     }
+
+    implementation("net.kyori:option:1.1.0")
 
     compileOnly("org.projectlombok:lombok:1.18.36")
     annotationProcessor("org.projectlombok:lombok:1.18.36")
